87fe25b448d6aa1c126c4500a5edd8eb4a314c48
hareesha <hareesha1906@gmail.com>
2016-07-20 14:07:28 -0700

    updated ActiveMQ broker instantiation

--- src/main/java/com/activeMQ/springJms/BaseServlet.java
+++ src/main/java/com/activeMQ/springJms/BaseServlet.java
@@ -2,7 +2,9 @@ package com.activeMQ.springJms;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
+import javax.jms.JMSException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.activemq.broker.BrokerFactory;
 import org.apache.activemq.broker.BrokerService;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class BaseServlet extends HttpServlet {
@@ -29,8 +30,7 @@ public class BaseServlet extends HttpServlet {
 			//WebapplicationContext
 			appCtx = WebApplicationContextUtils
 				        .getRequiredWebApplicationContext(getServletContext());
-			BrokerService broker = BrokerFactory.createBroker(new URI("broker:(tcp://localhost:61616)"));
-	        broker.start();
+			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
@@ -45,23 +45,37 @@ public class BaseServlet extends HttpServlet {
 			throws ServletException,IOException{
 		System.out.println("Inside service");
 		
-		//Producer
-		producer = (SpringJmsProducer) appCtx.getBean("springJmsProducer", SpringJmsProducer.class);
-		String msg = "This is from Spring JMS producer";
-		producer.sendMessage(msg);
-		System.out.println("Message sent check ActiveMQ queue");
-		
-		//Consumer
-		consumer = (SpringJmsConsumer)appCtx.getBean("springJmsConsumer", SpringJmsConsumer.class);
-		consumer.receiveMsg();
-		System.out.println("end of service");
-	}
-	
-	public void destroy(){
-		try {
+		try{
+			//to escape the JVM_Bind exception , instantiating the broker 
+			//object inside service method..Here broker is instantiated for each and every request
+			broker = BrokerFactory.createBroker(new URI("broker:(tcp://localhost:61616)"));
+	        broker.start();
+			
+			//Producer
+			producer = (SpringJmsProducer) appCtx.getBean("springJmsProducer", SpringJmsProducer.class);
+			String msg = "This is from Spring JMS producer";
+			producer.sendMessage(msg);
+			System.out.println("Message sent check ActiveMQ queue");
+			
+			//Consumer
+			consumer = (SpringJmsConsumer)appCtx.getBean("springJmsConsumer", SpringJmsConsumer.class);
+			consumer.receiveMsg();
+			System.out.println("end of service");
 			broker.stop();
+		}catch(JMSException e){
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void destroy(){
+		/*try {
+			broker.stop();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
 	}
 }
